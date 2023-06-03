9e0c8532c8f8ebc93329fd2f6fd737702af05fd8
Jakub Szulc <j.szulc.328@studms.ug.edu.pl>
2023-06-03 12:45:00 +0200

    lab12 fix

--- lab12/src/main/scala/Main.scala
+++ lab12/src/main/scala/Main.scala
@@ -1,66 +1,69 @@
 import akka.actor.{ActorSystem, Actor, ActorLogging, ActorRef, Props}
 
+class Boss extends Actor with ActorLogging{
+  var nadzorca : ActorRef = _
+  def receive: Receive = {
+    case Oblicz(x)=>{
+      if(context.child("nadzorca").isEmpty){
+        nadzorca = context.actorOf(Props(new Nadzorca()), "nadzorca")
+      }
+      nadzorca ! Oblicz(x)
+    }
+    case Wynik(x, wynik)=>{
+      log.info(s"${x} ist gleich ${wynik}")
+    }
+  }
+}
+
 case class Oblicz(n: Int)
+
+class Nadzorca(var cache: Map[Int, BigInt] = Map(1 -> 1, 2 -> 1), doZrobienia: Set[Int] = Set()) extends Actor with ActorLogging {
+  var pracownik : ActorRef = _
+  def receive: Receive ={
+    case Oblicz(x) =>{
+      if(context.child("pracownik").isEmpty){
+        pracownik = context.actorOf(Props(new Pracownik(x)), "pracownik")
+      }
+      if(cache.contains(x)){
+        log.info(s"Dieses Element ${x} wurde bereits berechnet")
+        context.parent ! Wynik(x, cache(x))
+      }else{
+        pracownik ! Oblicz(x)
+      }
+    }
+    case Wynik(x, wynik) =>{
+      var nowyCache = cache + (x -> wynik)
+      cache = nowyCache
+      context.parent ! Wynik(x, wynik)
+    }
+  }
+}
+
 case class Wynik(n: Int, fib: BigInt)
 
-
-class Boss extends Actor with ActorLogging {
-    var nadzorca : ActorRef = _
-    def receive : Receive ={
-        case Oblicz(liczba) => {
-            if(context.child("nadzorca").isEmpty){
-                nadzorca = context.actorOf(Props(new Nadzorca()), "nadzorca")
-            }
-            nadzorca ! Oblicz(liczba)
-        }
-        case Wynik(który, elem) => {
-            log.info(s"Element ${który} jest rowny ${elem}")
-        }
-    }
-}
-var pracownik : ActorRef = _
-class Nadzorca(var cache: Map[Int, BigInt] = Map(1 -> 1, 2 -> 1), doZrobienia: Set[Int] = Set()) extends Actor with ActorLogging {
-    def receive : Receive = {
-        case Oblicz(liczba) => {
-            if(cache.contains(liczba)){
-                log.info(s"Element ${liczba} zostal juz kiedys obliczony")
-                context.parent ! Wynik(liczba, cache(liczba))
-            }else{
-                if(context.child("pracownik").isEmpty){
-                    pracownik = context.actorOf(Props(new Pracownik(liczba)), "pracownik")
-                }
-                pracownik ! Oblicz(liczba)
-            }
-        }
-        case Wynik(który, liczba) => {
-            val nowaMapa1 = cache + (który-> liczba)
-            cache = nowaMapa1
-            println(cache)
-            context.parent ! Wynik(który, liczba)
-        }
-
-    }
-
-}
-
 class Pracownik(k: Int) extends Actor with ActorLogging {
-    def receive : Receive = {
-        case Oblicz(elem) =>{
-            def fibonacciRecursive(n: Int): Int = {
-                if(n<1) 0
-                else if (n == 1) 1
-                else fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2)
-            }
-            context.parent ! Wynik(elem,fibonacciRecursive(elem))
-        }
+  def fibonacci(n: Int): BigInt = {
+      @annotation.tailrec
+      def loop(n: Int, prev: BigInt, cur: BigInt): BigInt =
+        if (n <= 0) prev
+        else loop(n - 1, cur, prev + cur)
+      loop(n, 0, 1)
     }
+
+  def receive: Receive ={
+    case Oblicz(x)=>
+      context.parent ! Wynik(x, fibonacci(x))
+  }
 }
 
-@main def zadaniećwiczenia : Unit = {
-    val system = ActorSystem("Fibonacci")
-    val szef = system.actorOf(Props(new Boss()), "szef")
-    szef ! Oblicz(9)
-    szef ! Oblicz(10)
-    Thread.sleep(2000)
-    szef ! Oblicz(9)
+@main
+def dupajaja : Unit = {
+  val system = ActorSystem("Fibonacci")
+  val kierownik = system.actorOf(Props(new Boss()), "kierownik")
+  kierownik ! Oblicz(12)
+  kierownik ! Oblicz(2)
+  kierownik ! Oblicz(3)
+  Thread.sleep(500)
+  kierownik ! Oblicz(12)
+
 }
