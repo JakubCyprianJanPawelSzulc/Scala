d31323042bf0d7e31dce2c6ff677552987825120
Jakub Szulc <j.szulc.328@studms.ug.edu.pl>
2023-06-12 13:09:11 +0200

    lab12 fix

--- lab12/src/main/scala/Main.scala
+++ lab12/src/main/scala/Main.scala
@@ -1,63 +1,77 @@
 import akka.actor.{ActorSystem, Actor, ActorLogging, ActorRef, Props}
 
-class Boss extends Actor with ActorLogging{
-  var nadzorca : ActorRef = _
-  def receive: Receive = {
-    case Oblicz(x)=>{
-      if(context.child("nadzorca").isEmpty){
-        nadzorca = context.actorOf(Props(new Nadzorca()), "nadzorca")
-      }
-      nadzorca ! Oblicz(x)
+class Boss extends Actor with ActorLogging {
+  def receive : Receive ={
+    case Oblicz(n) => {
+      val nadzorca = context.actorOf(Props(new Nadzorca()), "kierownik")
+      nadzorca ! Oblicz(n)
+      context.become(zKierownikiem(nadzorca))
     }
-    case Wynik(x, wynik)=>{
-      log.info(s"${x} ist gleich ${wynik}")
+  }
+  def zKierownikiem(nadzorca: ActorRef): Receive = {
+    case Oblicz(n) => {
+      nadzorca ! Oblicz(n)
+    }
+    case Wynik(n, fib) => {
+      log.info(s"fib($n) = $fib")
     }
   }
 }
 
 case class Oblicz(n: Int)
 
-class Nadzorca(var cache: Map[Int, BigInt] = Map(1 -> 1, 2 -> 1), doZrobienia: Set[Int] = Set()) extends Actor with ActorLogging {
-  var pracownik : ActorRef = _
-  def receive: Receive ={
-    case Oblicz(x) =>{
-      if(context.child("pracownik").isEmpty){
-        pracownik = context.actorOf(Props(new Pracownik(x)), "pracownik")
-      }
-      if(cache.contains(x)){
-        log.info(s"Dieses Element ${x} wurde bereits berechnet")
-        context.parent ! Wynik(x, cache(x))
+class Nadzorca(cache: Map[Int, BigInt] = Map(1 -> 1, 2 -> 1), doZrobienia: Set[Int] = Set()) extends Actor with ActorLogging {
+  def receive : Receive ={
+    case Oblicz(n) => {
+      if(cache.contains(n)){
+        log.info(s"$n już było obliczone")
+        sender() ! Wynik(n, cache(n))
       }else{
-        pracownik ! Oblicz(x)
+        val pracownik = context.actorOf(Props(new Pracownik(n)), s"pracownik$n")
+        pracownik ! Oblicz(n)
+        context.become(zPracownikiem(pracownik, cache, doZrobienia))
       }
-    }
-    case Wynik(x, wynik) =>{
-      var nowyCache = cache + (x -> wynik)
-      cache = nowyCache
-      context.parent ! Wynik(x, wynik)
     }
   }
+  def zPracownikiem(pracownik: ActorRef, cache: Map[Int, BigInt], doZrobienia: Set[Int]): Receive = {
+    case Oblicz(n) => {
+      if(cache.contains(n)){
+        log.info(s"$n już było obliczone")
+        sender() ! Wynik(n, cache(n))
+      }else{
+        pracownik ! Oblicz(n)
+      }
+    }
+    case Wynik(n, fib) =>{
+      val nowyCache = cache + (n -> fib)
+      context.parent ! Wynik(n, fib)
+      context.become(zPracownikiem(pracownik, nowyCache, doZrobienia))
+    }
+    
+  }
+  
 }
 
 case class Wynik(n: Int, fib: BigInt)
 
 class Pracownik(k: Int) extends Actor with ActorLogging {
-  def fibonacci(n: Int): BigInt = {
-      @annotation.tailrec
-      def loop(n: Int, prev: BigInt, cur: BigInt): BigInt =
-        if (n <= 0) prev
-        else loop(n - 1, cur, prev + cur)
-      loop(n, 0, 1)
-    }
-
   def receive: Receive ={
-    case Oblicz(x)=>
-      context.parent ! Wynik(x, fibonacci(x))
+    case Oblicz(n) => {
+      val wynik = fib(n)
+      sender() ! Wynik(n, wynik)
+    }
   }
+  def fib (n: Int): BigInt = {
+      if(n == 1 || n == 2){
+        1
+      }else{
+        fib(n-1) + fib(n-2)
+      }
+    }
 }
 
 @main
-def lab12 : Unit = {
+def dupajaja : Unit = {
   val system = ActorSystem("Fibonacci")
   val kierownik = system.actorOf(Props(new Boss()), "kierownik")
   kierownik ! Oblicz(12)
